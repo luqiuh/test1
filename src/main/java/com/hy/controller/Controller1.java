be9594e7766ec43be889fb18da57ba2f076d1aa2
luqiuhong <3065790158@qq.com>
2019-03-15 12:20:41 +0800

    提交 m

---
+++ src/main/java/com/hy/controller/Controller1.java
@@ -0,0 +1,22 @@
+package com.hy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @Auther: asus
+ * @Date: 2019/3/14 16:50
+ * @Description:
+ */
+@Controller
+@RequestMapping("/student")
+public class Controller1 {
+    @RequestMapping("/query.action")
+    public ModelAndView query() {
+        ModelAndView modelAndView = new ModelAndView();
+        System.out.println("你想干嘛");
+        modelAndView.setViewName("/list.jsp");
+        return modelAndView;
+    }
+}
